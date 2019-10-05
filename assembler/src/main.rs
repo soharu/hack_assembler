bb23ee3c3af27e821f00fae582cba615d3a9485c
Jahyun Oh <soharu1024@gmail.com>
2019-10-05 22:34:18 +0900

    Add parser crate (wip)

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,8 +1,20 @@
+use std::env;
+use std::process;
 use regex::Regex;
 use code;
+use parser;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = parser::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = parser::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
 #[derive(PartialEq, Debug)]
---
+++ parser/src/lib.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use std::error::Error;
+
+pub struct Config {
+    filename: String
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+        let filename = args[1].clone();
+        Ok(Config { filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
