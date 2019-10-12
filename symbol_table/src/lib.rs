cdf1d3226447ef7b6c67d6488733b3c4331564d9
Jahyun Oh <soharu1024@gmail.com>
2019-10-12 15:22:41 +0900

    Add `get_address` method

--- symbol_table/src/lib.rs
+++ symbol_table/src/lib.rs
@@ -18,6 +18,12 @@ impl SymbolTable {
     pub fn contains(&self, symbol: &str) -> bool {
         self.symbol_map.contains_key(symbol)
     }
+
+    pub fn get_address(&self, symbol: &str) -> Option<i16> {
+        self.symbol_map
+            .get(symbol)
+            .map_or(None, |v| Some(v.clone()))
+    }
 }
 
 #[cfg(test)]
@@ -25,12 +31,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_symbol_table() {
         let mut symbol_table = SymbolTable::new();
         symbol_table.add_entry("SP", 0);
         symbol_table.add_entry("LCL", 1);
+
         assert_eq!(true, symbol_table.contains("SP"));
+        assert_eq!(Some(0), symbol_table.get_address("SP"));
+
         assert_eq!(true, symbol_table.contains("LCL"));
+        assert_eq!(Some(1), symbol_table.get_address("LCL"));
+
         assert_eq!(false, symbol_table.contains("ABC"));
+        assert_eq!(None, symbol_table.get_address("ABC"));
     }
 }
